b4d21816004e955293cf6a97c5112810b7fa6766
cecisalof <cecilia.sanchezlosada@gmail.com>
2025-05-11 10:05:20 +0200

    [Fix]: avoid header variable as null  when page loads

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -21,18 +21,3 @@ links.forEach(link => {
     link.classList.remove('active');
   }
 });
-
-// Add event listener to change navbar background on scroll
-const header = document.querySelector('header');
-const headerHeight = header.offsetHeight;
-
-window.addEventListener('scroll', function () {
-  const navbar = document.getElementById('navbar');
-  if (window.scrollY > headerHeight - 20) {
-    navbar.classList.remove('bg-transparent');
-    navbar.classList.add('bg-scroll');
-  } else {
-    navbar.classList.remove('bg-scroll');
-    navbar.classList.add('bg-transparent');
-  }
-});
